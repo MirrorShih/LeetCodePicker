a70c5023e90b830c8f0c57db42ce571d109cc316
Mirror <jess2529156651@gmail.com>
2022-09-07 16:36:22 +0800

    feat: pick a new problem when dislikes are more than likes

--- main.py
+++ main.py
@@ -56,7 +56,26 @@ if __name__ == '__main__':
             }
             r = requests.post("https://leetcode.com/graphql", json=data)
             question = r.json()["data"]["problemsetQuestionList"]["questions"][0]
-            if not question["paidOnly"] and doneDict.get(question["title"]) is None:
+            data = {"operationName": "questionData",
+                    "variables": {"titleSlug": question["titleSlug"]},
+                    "query": "query questionData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    "
+                             "questionId\n    questionFrontendId\n    boundTopicId\n    title\n    titleSlug\n    "
+                             "content\n    translatedTitle\n    translatedContent\n    isPaidOnly\n    difficulty\n   "
+                             " likes\n    dislikes\n    isLiked\n    similarQuestions\n    exampleTestcases\n    "
+                             "categoryTitle\n    contributors {\n      username\n      profileUrl\n      avatarUrl\n  "
+                             "    __typename\n    }\n    topicTags {\n      name\n      slug\n      translatedName\n  "
+                             "    __typename\n    }\n    companyTagStats\n    codeSnippets {\n      lang\n      "
+                             "langSlug\n      code\n      __typename\n    }\n    stats\n    hints\n    solution {\n   "
+                             "   id\n      canSeeDetail\n      paidOnly\n      hasVideoSolution\n      "
+                             "paidOnlyVideo\n      __typename\n    }\n    status\n    sampleTestCase\n    metaData\n  "
+                             "  judgerAvailable\n    judgeType\n    mysqlSchemas\n    enableRunCode\n    "
+                             "enableTestMode\n    enableDebugger\n    envInfo\n    libraryUrl\n    adminUrl\n    "
+                             "challengeQuestion {\n      id\n      date\n      incompleteChallengeCount\n      "
+                             "streakCount\n      type\n      __typename\n    }\n    __typename\n  }\n}\n"}
+            r = requests.post("https://leetcode.com/graphql", json=data)
+            likes = r.json()["data"]["question"]["likes"]
+            dislikes = r.json()["data"]["question"]["dislikes"]
+            if not question["paidOnly"] and doneDict.get(question["title"]) is None and likes > dislikes:
                 num -= 1
                 problems.append((question["title"], question["titleSlug"], question["frontendQuestionId"]))
                 if num == 0:
